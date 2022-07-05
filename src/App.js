2ea063caba53145b46d91db67594fdd435630df7
Gabriel <gopxfs@hotmail.com>
2022-07-05 13:56:32 -0300

    Add routers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import './index.scss';
 
 class App extends React.PureComponent {
   render() {
-    return (<Calculator />);
+    return (
+      <Routes>
+        <Route path="/" element={<Calculator />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Calculator />} />
+      </Routes>
+    );
   }
 }
 
