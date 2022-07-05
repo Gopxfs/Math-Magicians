8a174aacd3ca55c73bc5dbd6936b06353c8daab0
Gabriel <gopxfs@hotmail.com>
2022-07-05 17:14:15 -0300

    Add styling to quote

--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,5 +1,5 @@
 const QuotePage = () => (
-  <div>
+  <div className="quotePage">
     <h2>
       Mathematics is not about numbers, equations, computations, or algorithms:
       it is about understanding. -William Paul Thurston
