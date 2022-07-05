c14a0cfd03c95fe60c751168cc4c7a0c55f03f1c
Gabriel <gopxfs@hotmail.com>
2022-07-05 16:53:07 -0300

    Add home text & styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 const HomePage = () => (
-  <div>
-    <h1> Home </h1>
-    <h2> Welcome to our page! </h2>
-    <p> Text explaining the website </p>
+  <div className="homepage">
+    <h1> Welcome to my calculator project </h1>
+    <p>
+      This website can make simple math calculations in the &quot;Calculator&quot;
+      section.
+      <br />
+      In the &quot;Quote&quot; section, you can find a random mathematic-related quote!
+      <br />
+      Everything was done by using
+      <span> React </span>
+      (hooks, functional components & router).
+      If you like this website, feel free to give it a star on
+      <a href="https://github.com/Gopxfs/Math-Magicians"> Github </a>
+      and check my other
+      <a href="https://github.com/Gopxfs"> projects</a>
+      !
+    </p>
   </div>
 );
 
